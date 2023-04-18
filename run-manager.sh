f2aa6b76dac55ff19c8b3ecf76bd21f2373de806
diogonapoles <napolesdr@gmail.com>
2023-04-18 23:44:22 +0100

    fix: fixed run-manager scripts

--- run-manager.sh
+++ run-manager.sh
@@ -5,4 +5,4 @@
 export BASE_CP=base.app.manager.console/target/base.app.manager.console-1.4.0-SNAPSHOT.jar:base.app.manager.console/target/dependency/*;
 
 #REM call the java VM, e.g,
-java -cp $BASE_CP eapli.base.app.manager.console.BaseBackoffice
+java -cp $BASE_CP eapli.base.app.manager.console.BaseManager
