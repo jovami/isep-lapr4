dffdf2430073a72c6218de1a17b98f0a3521769c
1210954 <123769432+1210954@users.noreply.github.com>
2023-05-15 22:17:36 +0100

    fix(CourseRepository): use JPQL queries in ofState methods

--- base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaCourseRepository.java
+++ base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaCourseRepository.java
@@ -1,31 +1,24 @@
 package eapli.base.persistence.impl.jpa;
 
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 import eapli.base.course.domain.Course;
 import eapli.base.course.domain.CourseState;
 import eapli.base.course.repositories.CourseRepository;
 
-public class JpaCourseRepository extends BaseJpaRepositoryBase<Course,Long,Integer> implements CourseRepository {
+public class JpaCourseRepository extends BaseJpaRepositoryBase<Course, Long, Integer> implements CourseRepository {
+
     JpaCourseRepository(String persistenceUnitName) {
         super(persistenceUnitName, "code");
     }
 
-    // FIXME: find how to use JPQL query for this method
     @Override
     public Iterable<Course> ofStates(Set<CourseState> states) {
-        return StreamSupport.stream(findAll().spliterator(), false)
-                            .filter((course) -> states.contains(course.state()))
-                            .collect(Collectors.toList());
+        return match("e.state IN :states", "states", states);
     }
 
-    // FIXME: find how to use JPQL query for this method
     @Override
     public Iterable<Course> ofState(CourseState state) {
-        return StreamSupport.stream(findAll().spliterator(), false)
-                            .filter((course) -> course.state() == state)
-                            .collect(Collectors.toList());
+        return match("e.state = :state", "state", state);
     }
 }
