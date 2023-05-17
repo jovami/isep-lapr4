57700a97bd45dc0bb3e7a79c177bff24ba1cc1ce
1210951 <1210951@isep.ipp.pt>
2023-05-17 11:45:31 +0100

    feat/us1005:UI

--- base.app.manager.console/src/main/java/eapli/base/app/manager/console/presentation/SetCourseTeacherUI.java
+++ base.app.manager.console/src/main/java/eapli/base/app/manager/console/presentation/SetCourseTeacherUI.java
@@ -1,9 +1,11 @@
 package eapli.base.app.manager.console.presentation;
 
+import eapli.base.clientusermanagement.domain.users.Teacher;
 import eapli.base.course.application.SetCourseTeachersController;
+import eapli.base.course.domain.Course;
+import eapli.base.course.domain.StaffMember;
 import eapli.framework.presentation.console.AbstractUI;
-
-import eapli.framework.io.util.Console;
+import eapli.framework.presentation.console.SelectWidget;
 
 public class SetCourseTeacherUI extends AbstractUI {
 
@@ -11,13 +13,34 @@ public class SetCourseTeacherUI extends AbstractUI {
 
     @Override
     protected boolean doShow() {
-        ctrl.listCourses();
-        int id=Console.readInteger("Choose id course: ");
-        if(ctrl.chooseCourse(id)){
 
-        }else {
-            System.out.println("There is no course with the given id");
+        SelectWidget<Course> selecCourse = new SelectWidget<>("Choose Course", ctrl.courses());
+        selecCourse.show();
+        ctrl.chooseCourse(selecCourse.selectedElement());
+
+        boolean headTeacherCoosed = false;
+        while(!headTeacherCoosed){
+            SelectWidget<Teacher> selec = new SelectWidget<>("Choose Head Teacher", ctrl.teachers());
+            selec.show();
+            if(ctrl.chooseHeadTeacher(selec.selectedElement())){
+                headTeacherCoosed=true;
+            }else {
+                System.out.println("There was an error choosing the head teacher");
+            }
         }
+        SelectWidget<Teacher> selecMember = new SelectWidget<>("Choose new staff member", ctrl.teachers());
+        selecMember.show();
+
+        if(selecMember.selectedOption()!=0) {
+            if (ctrl.addStaffMember(selecMember.selectedElement())) {
+                System.out.println("There was an error with the new staff member");
+            } else {
+                System.out.println("Staff member added with succes");
+            }
+        }
+        for (StaffMember member: ctrl.staff()) {
+            System.out.println(member);
+        };
         return true;
     }
 
