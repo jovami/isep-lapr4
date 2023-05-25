80adb461ba7849e6ca96dbead59b3fbfa0508da9
1210951 <1210951@isep.ipp.pt>
2023-05-25 18:34:56 +0100

    fixes #54 : teachers that are already in the staff not being showed

--- base.app.manager.console/src/main/java/eapli/base/app/manager/console/presentation/SetCourseTeacherUI.java
+++ base.app.manager.console/src/main/java/eapli/base/app/manager/console/presentation/SetCourseTeacherUI.java
@@ -6,6 +6,8 @@ import eapli.base.course.domain.Course;
 import eapli.framework.presentation.console.AbstractUI;
 import eapli.framework.presentation.console.SelectWidget;
 
+import java.util.List;
+
 public class SetCourseTeacherUI extends AbstractUI {
 
     private SetCourseTeachersController ctrl = new SetCourseTeachersController();
@@ -17,9 +19,15 @@ public class SetCourseTeacherUI extends AbstractUI {
         selecCourse.show();
         ctrl.chooseCourse(selecCourse.selectedElement());
 
+        List<Teacher> teachers = (List<Teacher>) ctrl.teachers();
+        if(teachers==null){
+            System.out.println("There are no teachers available at this moment");
+            return false;
+        }
+
         boolean headTeacherCoosed = false;
         while (!headTeacherCoosed) {
-            SelectWidget<Teacher> selec = new SelectWidget<>("Choose Head Teacher", ctrl.teachers());
+            SelectWidget<Teacher> selec = new SelectWidget<>("Choose Head Teacher",teachers);
             selec.show();
             if (ctrl.chooseHeadTeacher(selec.selectedElement())) {
                 headTeacherCoosed = true;
@@ -28,27 +36,34 @@ public class SetCourseTeacherUI extends AbstractUI {
             }
         }
 
-        SelectWidget<Teacher> selecMember = new SelectWidget<>("Choose new staff member", ctrl.teachers());
         do {
-            selecMember.show();
-            if (selecMember.selectedOption() != 0) {
-                if (ctrl.addStaffMember(selecMember.selectedElement())) {
-                    System.out.println("Staff member added with succes");
-                } else {
-                    System.out.println("There was an error with the new staff member");
-                }
-            }
-
-        } while (selecMember.selectedOption() != 0);
+        } while (selectStaffMember());
 
         System.out.println("\n\tSTAFF MEMBERS:");
+        System.out.println(" - "+ctrl.headTeacher()+" (Head teacher)");
         for (Teacher member : ctrl.staff()) {
             System.out.println(" - " + member.acronym());
         }
-        ;
         return true;
     }
 
+    public boolean selectStaffMember(){
+        if(ctrl.teachers()==null){
+            System.out.println("\n\tThere are no more teachers available");
+            return false;
+        }
+        SelectWidget<Teacher>selecMember = new SelectWidget<>("Choose new staff member", ctrl.teachersNotInStaff());
+        selecMember.show();
+        if (selecMember.selectedOption() != 0) {
+            if (ctrl.addStaffMember(selecMember.selectedElement())) {
+                System.out.println("\n\tStaff member added with succes\n");
+            } else {
+                System.out.println("\n\tThere was an error with the new staff member\n");
+            }
+            return true;
+        }
+        return false;
+    }
     @Override
     public String headline() {
         return "Set Course Teachers";
--- base.core/src/main/java/eapli/base/course/application/SetCourseTeachersController.java
+++ base.core/src/main/java/eapli/base/course/application/SetCourseTeachersController.java
@@ -1,40 +1,53 @@
 package eapli.base.course.application;
 
-import static org.eclipse.collections.impl.block.factory.HashingStrategies.fromFunction;
-
-import java.util.Set;
-
-import org.eclipse.collections.impl.factory.HashingStrategySets;
-
 import eapli.base.clientusermanagement.domain.users.Teacher;
-import eapli.base.clientusermanagement.repositories.TeacherRepository;
 import eapli.base.course.domain.Course;
 import eapli.base.course.domain.StaffMember;
 import eapli.base.course.repositories.CourseRepository;
 import eapli.base.course.repositories.StaffRepository;
 import eapli.base.infrastructure.persistence.PersistenceContext;
 import eapli.framework.application.UseCaseController;
+import org.eclipse.collections.impl.factory.HashingStrategySets;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import static org.eclipse.collections.impl.block.factory.HashingStrategies.fromFunction;
 
 @UseCaseController
 public class SetCourseTeachersController {
     private final CourseRepository repo;
-    private final TeacherRepository teacherRepo;
     private final StaffRepository staffRepo;
     private final Set<Teacher> staff;
 
     // FIXME: use DTO
+    private ArrayList<Teacher> teachers = new ArrayList<>();
     private Course course;
 
     public SetCourseTeachersController() {
         repo = PersistenceContext.repositories().courses();
-        teacherRepo = PersistenceContext.repositories().teachers();
         staffRepo = PersistenceContext.repositories().staffs();
-
         this.staff = HashingStrategySets.mutable.with(fromFunction(Teacher::identity));
+    }
+
+    public ArrayList<Teacher> teachersNotInStaff(){
+        //collections
+        teachers = (ArrayList<Teacher>) PersistenceContext.repositories().teachers().findAll();
+        for (Teacher tch: staff()) {
+            teachers.remove(tch);
+        }
+        if (course.headTeacher().isPresent()){
+            teachers.remove(course.headTeacher().get());
+        }
+        return teachers;
+    }
+    public Iterable<Teacher> staff() {
+        return staffRepo.findByCourse(course);
     }
 
     public void chooseCourse(Course course) {
         this.course = course;
+        teachersNotInStaff();
     }
 
     public Iterable<Course> courses() {
@@ -43,24 +56,36 @@ public class SetCourseTeachersController {
 
     public boolean chooseHeadTeacher(Teacher teacher) {
         course.setHeadTeacher(teacher);
+        this.teachers.remove(teacher);
         return repo.save(course) != null;
     }
 
     public Iterable<Teacher> teachers() {
-        return teacherRepo.findAll();
+        if (teachers.isEmpty()){
+            return null;
+        }
+        return teachers;
     }
 
     public boolean addStaffMember(Teacher teacher) {
+
         boolean notHead = course.headTeacher()
                 .map(t -> !teacher.sameAs(t))
                 .orElse(false);
 
-        if (staff.add(teacher) && notHead)
+        if (staff.add(teacher) && notHead) {
+            this.teachers.remove(teacher);
             return staffRepo.save(new StaffMember(course, teacher)) != null;
+        }
         return false;
     }
 
-    public Iterable<Teacher> staff() {
-        return staffRepo.findByCourse(course);
+    public String headTeacher() {
+        if(course.headTeacher().isPresent()){
+            return course.headTeacher().get().acronym().toString();
+        }
+        return null;
+
     }
+
 }
