f7f974bad7fa631e5909d423e32db7eeee5d4002
1210954 <123769432+1210954@users.noreply.github.com>
2023-06-10 13:57:40 +0100

    fix(ListFutureExams): wrong parameter name in SQL query

--- base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaRegularExamRepository.java
+++ base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaRegularExamRepository.java
@@ -26,7 +26,7 @@ class JpaRegularExamRepository extends BaseJpaRepositoryBase<RegularExam, Long, 
     @Override
     public Iterable<RegularExam> examsOfCoursesAfterTime(LocalDateTime time, Set<Course> courses) {
         return match("e.course IN :courses AND e.regularExamDate.start > :time",
-                "courses", courses, "date", time);
+                "courses", courses, "time", time);
     }
 
     @Override
