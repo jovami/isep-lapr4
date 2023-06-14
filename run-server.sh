612100ebd902a69f6cfd0696c4417f7b92525783
1210954 <123769432+1210954@users.noreply.github.com>
2023-06-14 15:51:44 +0100

    fix(SharedBoards): update run-server/client scripts
    Fix main class path and use "$@" instead of specifying a set number of
    command-line arguments

--- run-server.sh
+++ run-server.sh
@@ -5,4 +5,4 @@
 export BASE_CP=base.app.board.web/target/base.app.board.web-1.4.0-SNAPSHOT.jar:base.app.board.web/target/dependency/*;
 
 #REM call the java VM, e.g,
-java -cp $BASE_CP eapli.board.SBPServerApp $1
+java -cp $BASE_CP eapli.board.server.SBPServerApp "$@"
