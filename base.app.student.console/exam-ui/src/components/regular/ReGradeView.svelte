4985126a7ebcef7b224fb70393301136ea810418
diogonapoles <napolesdr@gmail.com>
2023-06-17 18:51:48 +0100

    fix(us_2004): resolve issue preventing exam grading from showing question descriptions

--- base.app.student.console/exam-ui/src/components/regular/ReGradeView.svelte
+++ base.app.student.console/exam-ui/src/components/regular/ReGradeView.svelte
@@ -47,7 +47,7 @@
         <hr class="w-100 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-3 dark:bg-gray-500">
         <br />
         {#each section.answers as answer, j}
-            <h3>Question {j + 1} &mdash; {exam.sections[i].questions[j]}</h3>
+            <h3>Question {j + 1} &mdash; {exam.sections[i].questions[j].description}</h3>
             <p>
                 Points:
                 {#if answer.points == null}
