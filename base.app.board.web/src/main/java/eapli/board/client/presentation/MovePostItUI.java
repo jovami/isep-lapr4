578e9f2a95ea5661c34bf42244dd46885d7a04e0
diogonapoles <napolesdr@gmail.com>
2023-06-17 14:15:02 +0100

    refactor(us_3007): better ui for moving post it

--- base.app.board.web/src/main/java/eapli/board/client/presentation/MovePostItUI.java
+++ base.app.board.web/src/main/java/eapli/board/client/presentation/MovePostItUI.java
@@ -2,8 +2,6 @@ package eapli.board.client.presentation;
 
 import com.ibm.icu.impl.Pair;
 import eapli.board.client.application.MovePostItController;
-import eapli.board.client.application.UndoPostItLastChangeController;
-import eapli.board.client.dto.BoardRowColDTO;
 import eapli.board.client.dto.MovePostItDTO;
 import eapli.framework.io.util.Console;
 import eapli.framework.presentation.console.AbstractUI;
@@ -15,7 +13,7 @@ import java.net.InetAddress;
 import java.util.ArrayList;
 
 public class MovePostItUI extends AbstractUI {
-    private MovePostItController ctrl;
+    private final MovePostItController ctrl;
 
     public MovePostItUI(InetAddress serverIP, int serverPort) {
         super();
@@ -25,25 +23,52 @@ public class MovePostItUI extends AbstractUI {
     @Override
     protected boolean doShow() {
         var list = new ArrayList<String>();
+        var dimensions = new ArrayList<Pair<String, String>>();
         var boardRowColumn = ctrl.listBoardString();
         for (int i = 0; i < boardRowColumn.size(); i = i + 3) {
             list.add(boardRowColumn.get(i));
+            dimensions.add(Pair.of(boardRowColumn.get(i + 1), boardRowColumn.get(i + 2)));
         }
 
         var widget = new SelectWidget<>("Choose Board to view\n", list);
         widget.show();
-        if (widget.selectedOption() <= 0)
+        int idx = widget.selectedOption() - 1;
+        if ((idx + 1) <= 0)
             return false;
 
-        var boardName = widget.selectedElement();
         System.out.println("Moving from\n===========");
-        var rowFrom = Console.readInteger("Board row:");
-        var columnFrom = Console.readInteger("Board Col:");
-        System.out.println("Moving To\n=========");
-        var rowTo = Console.readInteger("Board row:");
-        var columnTo = Console.readInteger("Board Col:");
+        System.out.printf("[Dimensions: %s (rows) x %s (columns)]\n",
+                dimensions.get(idx).first, dimensions.get(idx).second);
 
-        var dto = new MovePostItDTO(boardName, rowFrom, columnFrom, rowTo, columnTo);
+        var rowFrom = Console.readInteger("-> Board row:");
+        if (rowFrom > Integer.parseInt(dimensions.get(idx).first) || rowFrom < 1) {
+            System.out.println("Invalid Row");
+            return false;
+        }
+
+        var columnFrom = Console.readInteger("-> Board column:");
+        if (columnFrom > Integer.parseInt(dimensions.get(idx).second) || columnFrom < 1) {
+            System.out.println("Invalid Column");
+            return false;
+        }
+
+        System.out.println("Moving To\n=========");
+        System.out.printf("[Dimensions: %s (rows) x %s (columns)]\n",
+                dimensions.get(idx).first, dimensions.get(idx).second);
+
+        var rowTo = Console.readInteger("-> Board row:");
+        if (rowTo > Integer.parseInt(dimensions.get(idx).first) || rowTo < 1) {
+            System.out.println("Invalid Row");
+            return false;
+        }
+
+        var columnTo = Console.readInteger("-> Board column:");
+        if (columnTo > Integer.parseInt(dimensions.get(idx).second) || columnTo < 1) {
+            System.out.println("Invalid Column");
+            return false;
+        }
+
+        var dto = new MovePostItDTO(widget.selectedElement(), rowFrom, columnFrom, rowTo, columnTo);
 
         try {
             this.ctrl.movePostIt(dto);
