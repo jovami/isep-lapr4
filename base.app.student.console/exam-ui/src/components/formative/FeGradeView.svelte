e1d0ec257acc0bdd222fc4bf4888c03183c3eded
1210954 <123769432+1210954@users.noreply.github.com>
2023-06-18 15:42:04 +0100

    fix(us_2004/2009): update grade view UI
    Final grade wasn't being properly rendered

--- base.app.student.console/exam-ui/src/components/formative/FeGradeView.svelte
+++ base.app.student.console/exam-ui/src/components/formative/FeGradeView.svelte
@@ -35,14 +35,20 @@
         Grade:
         {#if result.grade === result.maxGrade}
             <strong>{result.grade}</strong>
+        {:else}
+            {result.grade}
         {/if}
         / <strong>{result.maxGrade}</strong>
     </p>
-    <hr class="w-100 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-3 dark:bg-gray-500">
+    <hr
+        class="w-100 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-3 dark:bg-gray-500"
+    />
     <br />
     {#each result.sections as section, i}
         <h2>Section {i + 1}</h2>
-        <hr class="w-100 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-3 dark:bg-gray-500">
+        <hr
+            class="w-100 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-3 dark:bg-gray-500"
+        />
         <br />
         {#each section.answers as answer, j}
             <h3>
--- base.app.student.console/exam-ui/src/components/regular/ReGradeView.svelte
+++ base.app.student.console/exam-ui/src/components/regular/ReGradeView.svelte
@@ -40,14 +40,21 @@
             / <strong>{result.maxGrade}</strong>
         {/if}
     </p>
-    <hr class="w-100 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-3 dark:bg-gray-500">
+    <hr
+        class="w-100 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-3 dark:bg-gray-500"
+    />
     <br />
     {#each result.sections as section, i}
         <h2>Section {i + 1}</h2>
-        <hr class="w-100 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-3 dark:bg-gray-500">
+        <hr
+            class="w-100 h-1 mx-auto my-4 bg-gray-100 border-0 rounded md:my-3 dark:bg-gray-500"
+        />
         <br />
         {#each section.answers as answer, j}
-            <h3>Question {j + 1} &mdash; {exam.sections[i].questions[j].description}</h3>
+            <h3>
+                Question {j + 1} &mdash; {exam.sections[i].questions[j]
+                    .description}
+            </h3>
             <p>
                 Points:
                 {#if answer.points == null}
