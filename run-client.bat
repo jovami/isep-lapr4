0bcc58c10a2d9d1c47c1526711b34f0bf0e18fd2
1210951 <1210951@isep.ipp.pt>
2023-06-18 20:55:44 +0100

    feat(us_3005): fix bat files for server/client

--- run-client.bat
+++ run-client.bat
@@ -1,10 +1,8 @@
-REM set the class path,
+@echo off
 
-REM assumes the build was executed with maven copy-dependencies
-REM SET BASE_CP=base.app.board.web\target\base.app.board.web-1.4.0-SNAPSHOT.jar;base.app.board.web\target\dependency\*;
+set IP=%1
+set PORT=%2
 
 
-REM call the java VM, e.g,
-REM java -cp %BASE_CP% eapli.board.client.SBPClientApp %1 %2
+java -jar base.app.board.client\target\base.app.board.client-1.4.0-SNAPSHOT.jar %IP% 9000
 
-java -jar base.app.board.client/target/base.app.board.client-1.4.0-SNAPSHOT.jar %1 %2
--- run-server.bat
+++ run-server.bat
@@ -1,9 +1,3 @@
-REM set the class path,
+@echo off
 
-REM assumes the build was executed with maven copy-dependencies
-REM SET BASE_CP=base.app.board.web\target\base.app.board.web-1.4.0-SNAPSHOT.jar;base.app.board.web\target\dependency\*;
-
-
-REM call the java VM, e.g,
-REM java -cp %BASE_CP% eapli.board.server.SBServerApp %1
-java -jar base.app.board.server/target/base.app.board.server-1.4.0-SNAPSHOT.jar %1
+java -jar base.app.board.server\target\base.app.board.server-1.4.0-SNAPSHOT.jar 9000
